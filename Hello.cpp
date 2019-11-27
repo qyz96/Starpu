98a2ece44090a8610c1e86d9d3ce47cd56508905
Yizhou Qian <adncat@stanford.edu>
2019-11-27 14:16:06 -0800

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -5,6 +5,39 @@
 #include <Eigen/Cholesky>
 
 
+void potrf(void *buffers[], void *cl_arg) { LOG_TIMER("potrf"); }
+struct starpu_codelet potrf_cl = {
+    .where = STARPU_CPU,
+    .cpu_funcs = { potrf, NULL },
+    .nbuffers = 1,
+    .modes = { STARPU_RW }
+};
+
+void trsm(void *buffers[], void *cl_arg) { LOG_TIMER("trsm"); }
+struct starpu_codelet trsm_cl = {
+    .where = STARPU_CPU,
+    .cpu_funcs = { trsm, NULL },
+    .nbuffers = 2,
+    .modes = { STARPU_R, STARPU_RW }
+};
+
+void syrk(void *buffers[], void *cl_arg) { LOG_TIMER("syrk"); }
+struct starpu_codelet syrk_cl = {
+    .where = STARPU_CPU,
+    .cpu_funcs = { syrk, NULL },
+    .nbuffers = 2,
+    .modes = { STARPU_R, STARPU_RW }
+};
+
+void gemm(void *buffers[], void *cl_arg) { LOG_TIMER("gemm"); }
+struct starpu_codelet gemm_cl = {
+    .where = STARPU_CPU,
+    .cpu_funcs = { gemm, NULL },
+    .nbuffers = 3,
+    .modes = { STARPU_R, STARPU_R, STARPU_RW }
+};
+
+
 void cpu_func(void *buffers[], void *cl_arg)
 {
     printf("Hello world\n");
@@ -15,18 +48,44 @@ struct starpu_codelet cl =
     .nbuffers = 0
 };
 
+void test(void *buffers[], void *cl_arg)
+{
+	// get the current task
+	auto task = starpu_task_get_current();
+
+	// get the user data (pointers to the vec_A, vec_B, vec_C std::vector)
+	auto u_data0 = starpu_data_get_user_data(task->handles[0]); assert(u_data0);
+
+	// cast void* in std::vector<char>*
+	auto A = static_cast<MatrixXd*>(u_data0);
+
+
+	// all the std::vector have to have the same size
+	cout<<*A<<"\n";
+}
+
 //Test
 int main()
 {
+    int n=10;
+    int nb=10;
+    auto val = [&](int i, int j) { return 1/(float)((i-j)*(i-j)+1); };
+    MatrixXd *A;
+    *A = MatrixXd::NullaryExpr(n*nb,n*nb, val);
+    starpu_data_handle_t spu_T;
+    starpu_vector_data_register(&spu_T, STARPU_MAIN_RAM, (uintptr_t)A->data(), n, sizeof(double));
+    starpu_data_set_user_data(spu_vec_A, (void*)A);
+    starpu_codelet cl;
+	starpu_codelet_init(&cl);
+	cl.cpu_funcs     [0] = test;
+	cl.cpu_funcs_name[0] = "test";
+	cl.nbuffers          = 1;
+	cl.modes         [0] = STARPU_RW;
+	cl.name              = "potrf";
     /* initialize StarPU */
     starpu_init(NULL);
-    struct starpu_task *task = starpu_task_create();
-    task->cl = &cl; /* Pointer to the codelet defined above */
-    /* starpu_task_submit will be a blocking call. If unset,
-    starpu_task_wait() needs to be called after submitting the task. */
-    task->synchronous = 1;
-    /* submit the task to StarPU */
-    starpu_task_submit(task);
+    ret = starpu_task_insert(&cl, STARPU_RW, spu_T, 0);
+
     /* terminate StarPU */
     starpu_shutdown();
     return 0;
