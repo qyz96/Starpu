0dbcc4654bfec831022d1dd8da90d2fe3a560c74
Yizhou Qian <adncat@stanford.edu>
2019-12-16 18:03:38 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -119,12 +119,8 @@ static void s_potrf(int k, starpu_data_handle_t data)
 	{
 		starpu_tag_declare_deps(TAG11(k), 1, TAG22(k-1, k, k));
 	}
-    //starpu_task_submit(task);
-    int ret = starpu_task_insert(&potrf_cl,
-                         STARPU_PRIORITY, STARPU_MAX_PRIO,
-						 STARPU_RW, data,
-						 STARPU_TAG_ONLY, TAG11(k),
-						 0);
+    starpu_task_submit(task);
+    //int ret = starpu_task_insert(&potrf_cl,STARPU_PRIORITY, STARPU_MAX_PRIO,STARPU_RW, data,STARPU_TAG_ONLY, TAG11(k),0);
 }
 
 static void s_trsm(int k, int i, starpu_data_handle_t data1,  starpu_data_handle_t data2)
