e3958d54bcbc47a30432719441a5d57eb67f7b27
Yizhou Qian <adncat@stanford.edu>
2019-12-18 15:54:55 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -22,11 +22,8 @@ using namespace Eigen;
 
 
 void potrf(void *buffers[], void *cl_arg) { 
-	//auto task = starpu_task_get_current();
-	//auto u_data0 = starpu_data_get_user_data(task->handles[0]); 
-	//auto A = static_cast<MatrixXd*>(u_data0);
+
     MatrixXd *A= (MatrixXd *)STARPU_VARIABLE_GET_PTR(buffers[0]);
-    //printf("POTRF:%llx \n", task->tag_id);
 	LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'L', A->rows(), A->data(), A->rows());
      }
 struct starpu_codelet potrf_cl = {
@@ -37,11 +34,8 @@ struct starpu_codelet potrf_cl = {
 };
 
 void trsm(void *buffers[], void *cl_arg) {
-    auto task = starpu_task_get_current();
-	auto u_data0 = starpu_data_get_user_data(task->handles[0]); 
-	auto A0 = static_cast<MatrixXd*>(u_data0);
-    auto u_data1 = starpu_data_get_user_data(task->handles[1]); 
-	auto A1 = static_cast<MatrixXd*>(u_data1);
+	MatrixXd *A0= (MatrixXd *)STARPU_VARIABLE_GET_PTR(buffers[0]);
+	MatrixXd *A1= (MatrixXd *)STARPU_VARIABLE_GET_PTR(buffers[1]);
 	cblas_dtrsm(CblasColMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit, A0->rows(), 
     A0->rows(), 1.0, A0->data(),A0->rows(), A1->data(), A0->rows());
     //printf("TRSM:%llx \n", task->tag_id);
