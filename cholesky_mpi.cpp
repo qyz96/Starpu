0729052ae1b82f1a4c316034f39dfa114603ca38
Yizhou Qian <adncat@stanford.edu>
2019-12-19 18:48:06 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -63,18 +63,19 @@ void test(int rank)  {
         starpu_variable_data_register(&data2, -1, (uintptr_t)NULL, sizeof(int));
     }
     else {
-        starpu_variable_data_register(&data1, STARPU_MAIN_RAM, (uintptr_t)a, sizeof(int));
+        starpu_variable_data_register(&data1, -1, (uintptr_t)NULL, sizeof(int));
         starpu_variable_data_register(&data2, STARPU_MAIN_RAM, (uintptr_t)b, sizeof(int));
     }
     starpu_mpi_data_register(data1, 0, rank);
     starpu_mpi_data_register(data2, 1, rank);
-    starpu_tag_declare_deps(TAG11(1), 1, TAG11(2));
+
     if (rank==0) {
-        starpu_mpi_task_insert(MPI_COMM_WORLD,&cl1, STARPU_RW, data1, STARPU_TAG_ONLY, TAG11(1), 0);
+        starpu_mpi_task_insert(MPI_COMM_WORLD,&cl1, STARPU_RW, data1, 0);
     }
     else {
-        starpu_mpi_task_insert(MPI_COMM_WORLD,&cl2, STARPU_R, data1,STARPU_RW, data2, STARPU_TAG_ONLY, TAG11(2), 0);
+        starpu_mpi_task_insert(MPI_COMM_WORLD,&cl2, STARPU_R, data1,STARPU_RW, data2,0);
     }
+
 
     return;
 
