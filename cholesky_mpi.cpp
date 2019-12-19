b20f88cd58b6281bec71462e0bd996b6c9f816b6
Yizhou Qian <adncat@stanford.edu>
2019-12-19 17:24:04 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -138,7 +138,7 @@ void cholesky(int n, int nb, int rank, int size) {
                     starpu_variable_data_register(&dataA[ii+jj*nb], STARPU_MAIN_RAM, (uintptr_t)blocs[ii+jj*nb], sizeof(MatrixXd));
                 }
                 else {
-                    starpu_variable_data_register(&dataA[ii+jj*nb], -1, (uintptr_t)NULL, sizeof(MatrixXd));
+                    starpu_variable_data_register(&dataA[ii+jj*nb], STARPU_MAIN_RAM, (uintptr_t)blocs[ii+jj*nb], sizeof(MatrixXd));
                 }
                 if (dataA[ii+jj*nb]) {
                     starpu_mpi_data_register(dataA[ii+jj*nb], ii+jj*nb, rank);
