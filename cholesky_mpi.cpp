3fbc6a11f30e6d43e49931e7cfd929cc80c7f36d
Yizhou Qian <adncat@stanford.edu>
2019-12-19 13:46:34 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -149,22 +149,22 @@ void cholesky(int n, int nb, int rank, int size) {
     //starpu_init(NULL);
     double start = starpu_timing_now();
     for (int kk = 0; kk < nb; ++kk) {
-        if ((kk+kk*nb)%size == rank) {
+        if (1) {
             starpu_mpi_task_insert(MPI_COMM_WORLD,&potrf_cl,STARPU_RW, dataA[kk+kk*nb],STARPU_TAG_ONLY, TAG11(kk),0);
         }
         for (int ii = kk+1; ii < nb; ++ii) {
-            if ((ii+kk*nb)%size == rank) {
+            if (1) {
             starpu_mpi_task_insert(MPI_COMM_WORLD,&trsm_cl,STARPU_R, dataA[kk+kk*nb],STARPU_RW, dataA[ii+kk*nb],STARPU_TAG_ONLY, TAG21(kk,ii),0);
             }
             for (int jj=kk+1; jj < nb; ++jj) {         
                 if (jj <= ii) {
                     if (jj==ii) {
-                        if ((ii+jj*nb)%size == rank) {
+                        if ((1) {
                         starpu_mpi_task_insert(MPI_COMM_WORLD,&syrk_cl, STARPU_R, dataA[ii+kk*nb],STARPU_RW, dataA[ii+jj*nb],STARPU_TAG_ONLY, TAG22(kk,ii,jj),0);
                         }
                     }
                     else {
-                        if ((ii+jj*nb)%size == rank) {
+                        if ((1) {
                         starpu_mpi_task_insert(MPI_COMM_WORLD,&gemm_cl,STARPU_R, dataA[ii+kk*nb],STARPU_R, dataA[jj+kk*nb],STARPU_RW, dataA[ii+jj*nb],STARPU_TAG_ONLY, TAG22(kk,ii,jj),0);
                         }
                     }
