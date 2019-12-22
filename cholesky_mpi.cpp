9370bba0f782b3e8835ab0c53eef8ff4bd10d149
Yizhou Qian <adncat@stanford.edu>
2019-12-22 14:01:51 +0800

    Update cholesky_mpi.cpp

--- cholesky_mpi.cpp
+++ cholesky_mpi.cpp
@@ -175,7 +175,8 @@ void test(int rank)  {
 
     starpu_mpi_task_insert(MPI_COMM_WORLD,&cl1, STARPU_RW, data1, 0);
     starpu_mpi_task_insert(MPI_COMM_WORLD,&cl2, STARPU_R, data1,STARPU_RW, data2,0);
-    
+    starpu_data_unregister(data1);
+    starpu_data_unregister(data2);
 
    /*
     int nb=2;
