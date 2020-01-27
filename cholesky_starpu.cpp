b6ca6fc9f1620df91a9f5f3263446d4f2f11ff85
Yizhou Qian <adncat@stanford.edu>
2020-01-27 12:18:05 -0800

    Update cholesky_starpu.cpp

--- cholesky_starpu.cpp
+++ cholesky_starpu.cpp
@@ -145,7 +145,7 @@ int main(int argc, char **argv)
     int prov = -1;
     starpu_init(NULL);
     int rank, size;
-
+    rank=0;
     if (rank==0) {
         printf("Running on %d CPU cores per rank,", starpu_worker_get_count_by_type(STARPU_CPU_WORKER));
         printf("and %d ranks in total\n", size);
